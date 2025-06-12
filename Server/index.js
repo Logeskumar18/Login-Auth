81bfe0038144c4529bf3ed8d725599a3bbd86584
Logeskumar18 <kumarloges329@gmail.com>
2025-06-12 09:39:09 +0530

    Day 2 Commits

--- Server/index.js
+++ Server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import connectDB from './config/mongodb.js';
 
 const app = express();
 const port = process.env.PORT ||  4000;
+
+connectDB()
 
 app.use(cors({credentials: true}));
 app.use(express.json());
