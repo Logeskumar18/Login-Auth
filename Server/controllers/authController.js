df801ed24e17e8a282586342f2100e6bde576000
Loges <kumarloges329@gmail.com>
2025-06-13 18:06:19 +0530

    Added

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -173,9 +173,47 @@ export const sendVerifyOtp = async (req, res) => {
 
         await transporter.sendMail(mailOption)
 
-        res.json({success:true,message:'Verification OTP Sent on Email '})
+        res.json({ success: true, message: 'Verification OTP Sent on Email ' })
 
     } catch (error) {
         res.json({ success: false, message: error.message })
     }
+}
+
+
+export const verifyEmail = async (req, res) => {
+    const { userId, otp } = req.body;
+
+    if (!userId || !otp) {
+        res.json({ success: false, message: 'Missing Details' });
+    }
+    try {
+
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.json({ success: false, message: 'User Not Found' });
+        }
+
+        if (user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({ success: false, message: 'Invalid OTP' });
+        }
+
+        if(user.verifyOtpExpireAt < Date.now()){
+            return res.json({ success: false, message: 'OTP Expired' });  
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpireAt = 0;
+
+        await user.save();
+         
+        return res.json({success: true, message: 'Email Verification Successfully'});
+        
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+
 }
